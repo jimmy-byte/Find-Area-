2e20da6318868caf7614cfa4f412f25c1f5f5c81
Ishara Madushanka <47331470+ISHHMadushanka@users.noreply.github.com>
2020-10-09 09:47:53 +0530

    Update area.c

--- area.c
+++ area.c
@@ -2,10 +2,10 @@
 int main(void)
  { float  a;
 printf("Enter length of a side \n");
- scanf("%f", a);
-int area=a*a;
-char perimeter=4*a;
+ scanf("%f", &a);
+float area=a*a;
+float perimeter=4*a;
 printf("The Area = %f\n", area);
-printf("The Perimeter = f\n", perimeter);
+printf("The Perimeter = %f\n", perimeter);
 return 0;
 }
