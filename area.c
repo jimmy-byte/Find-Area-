4fd8e75662c1c46de4f416d46658df0e0d1516cc
Jimmy Nelson <72438702+jimmy-byte@users.noreply.github.com>
2020-10-09 09:34:37 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+ { float  a;
+printf("Enter length of a side \n");
+ scanf("%f", a);
+int area=a*a;
+char perimeter=4*a;
+printf("The Area = %f\n", area);
+printf("The Perimeter = f\n", perimeter);
+return 0;
+}
